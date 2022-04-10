74c8e88e1bec0c19be410c42e63554ef2e70f482
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-04-10 04:15:20 +0200

    initial gui

---
+++ TechHealth/View/DoctorView/CRUDAppointments/CreateExamination.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TechHealth.View.DoctorView.CRUDAppointments
+{
+    /// <summary>
+    /// Interaction logic for CreateExamination.xaml
+    /// </summary>
+    public partial class CreateExamination : Window
+    {
+        public CreateExamination()
+        {
+            InitializeComponent();
+        }
+    }
+}
