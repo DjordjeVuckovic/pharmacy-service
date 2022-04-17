acd85e59f3cd35fdebbbffc663b7c5c179ce33d9
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-04-17 20:49:49 +0200

    Medicine repo created

--- TechHealth/MainWindow.xaml.cs
+++ TechHealth/MainWindow.xaml.cs
@@ -25,7 +25,8 @@ namespace TechHealth
         public MainWindow()
         {
             InitializeComponent();
-           
+            
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -56,6 +57,70 @@ namespace TechHealth
         private void MainWindow_OnClosing(object sender, CancelEventArgs e)
         {
             InitializeComponent();
+        }
+
+        private void initMed()
+        {
+             /*InitializeComponent();
+            RandomGenerator randomGenerator = new RandomGenerator();
+            Substance substance = new Substance
+            {
+                SubstanceId = "113",
+                SubstanceName = "ibuprofen"
+            };
+            Substance substance1 = new Substance
+            {
+                SubstanceId = "114",
+                SubstanceName = "laktoza"
+            };
+            Substance substance2 = new Substance
+            {
+                SubstanceId = "115",
+                SubstanceName = "calcijum"
+            };
+            Substance substance3 = new Substance
+            {
+                SubstanceId = "116",
+                SubstanceName = "paracetamol"
+            };
+            List<Substance> substances = new List<Substance>();
+            substances.Add(substance);
+            substances.Add(substance1);
+            substances.Add(substance2);
+            Medicine medicine = new Medicine
+            {
+                MedicineId = randomGenerator.GenerateRandHash(),
+                Composition = substances,
+                Quantity = 50,
+                Units = "sirup",
+                SideEffects = "/",
+                MainSubstance = substance,
+                HarmfulRate = 0,
+                Price = 300,
+                MedicineName = "Brufen",
+                Allergens = "/",
+                Approved = true
+            };
+            List<Substance> substances1 = new List<Substance>();
+            substances1.Add(substance3);
+            substances1.Add(substance2);
+            Medicine medicine1 = new Medicine
+            {
+                MedicineId = randomGenerator.GenerateRandHash(),
+                Composition = substances1,
+                Quantity = 70,
+                Units = "pill",
+                SideEffects = "/",
+                MainSubstance = substance3,
+                HarmfulRate = 0,
+                Price = 100,
+                MedicineName = "Paracetamol Uno",
+                Allergens = "/",
+                Approved = false
+            };
+            MedicineRepository.Instance.Create(medicine);
+            MedicineRepository.Instance.Create(medicine1);*/
+
         }
     }
 }
--- TechHealth/obj/Debug/TechHealth_Content.g.i.cs
+++ TechHealth/obj/Debug/TechHealth_Content.g.i.cs
@@ -13,6 +13,7 @@
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/appointment.json")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/doctor.json")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/manager.json")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/medicine.json")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/patient.json")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/secretary.json")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("json/specialization.json")]
