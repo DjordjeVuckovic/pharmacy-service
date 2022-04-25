74623585d5f5aef5efaddc28b2e3259f7a48c576
Nenad Sredojević <n.sredojevic3@gmail.com>
2022-04-25 23:03:45 +0200

    Equipment VM

---
+++ TechHealth/View/ManagerView/ViewModel/EquipmentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHealth.Core;
+using TechHealth.Model;
+using TechHealth.Repository;
+
+namespace TechHealth.View.ManagerView.ViewModel
+{
+    public class EquipmentViewModel : ViewModelBase
+    {
+        private  ObservableCollection<Equipment> eqlist;
+
+        public ObservableCollection<Equipment> Equipment =>
+            eqlist ?? (eqlist =
+                new ObservableCollection<Equipment>(EquipmentRepository.Instance.DictionaryValuesToList()));
+
+        public EquipmentViewModel()
+        {
+        }
+    }
+}
