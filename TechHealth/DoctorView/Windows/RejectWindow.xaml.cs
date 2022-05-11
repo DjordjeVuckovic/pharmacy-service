3095b1e2cc5ada5cb562e88ca136d5a58322fdf5
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-05-11 10:34:06 +0200

    update project

---
+++ TechHealth/DoctorView/Windows/RejectWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TechHealth.DoctorView.Windows
+{
+    public partial class RejectWindow : Window
+    {
+        public RejectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
