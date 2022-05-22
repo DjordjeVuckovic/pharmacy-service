fcfa06cddfeda6237fdc0279a56650be4a5aaaf8
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-05-22 14:46:49 +0200

    sequnce is finished

--- TechHealth/Model/DoctorVacationRequest.cs
+++ TechHealth/Model/DoctorVacationRequest.cs
@@ -14,6 +14,7 @@ namespace TechHealth.Model
         
         public bool CheckDaysConflict(DoctorVacationRequest doctorVacationRequest)
         {
+            
             return doctorVacationRequest.StartDate < FinishDate && doctorVacationRequest.FinishDate > StartDate;
         }
         
