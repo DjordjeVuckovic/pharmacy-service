cd97fe398ac8964a78c4db6592eb98dba2df0aa2
Dusan Vukovic <vukovic.ra16.2019@uns.ac.rs>
2022-05-22 23:39:05 +0200

    Revert "Merge branch 'ocena21.5'"
    This reverts commit cce04ee39f3e2fd0ba98d4b36ac01938da44574c, reversing
    changes made to dc2e44b7f499bc07ddef1442d9772bbe324fb449.

--- TechHealth/View/PatientView/View/RateHospital.xaml.cs
+++ TechHealth/View/PatientView/View/RateHospital.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TechHealth.Core;
 using TechHealth.Model;
-using TechHealth.Repository;
 
 namespace TechHealth.View.PatientView.View
 {
@@ -24,46 +21,23 @@ namespace TechHealth.View.PatientView.View
     /// </summary>
     public partial class RateHospital : UserControl
     {
-        public HospitalGrade AnketaBolnica { get; set; }  //Ova funkcija mi da ocenu bolnice za jednog pacijenta, ne znam da li treba da napravim da moze da menja ocenu ili ne
-        public RelayCommand RateHospitalCommand { get; set; }
-        
-
+        //public HospitalGrade AnketaBolnica { get; set; }
         public RateHospital()
         {
             InitializeComponent();
-            AnketaBolnica = new HospitalGrade
-            {
-                Patient = PatientRepository.Instance.GetPatientbyId("2456") //zakucamo za sad
-            };
-            RateHospitalCommand = new RelayCommand(param => ExecuteRate());
             
         }
 
-        private void ExecuteRate()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            AnketaBolnica.Id = Guid.NewGuid().ToString("N");
-            AnketaBolnica.OcenaCistoca = BasicRatingBarClean.Value;
-            AnketaBolnica.OcenaDostupnost = BasicRatingBarAvailable.Value;
-            AnketaBolnica.OcenaLakoca = BasicRatingBarEasy.Value;
-            AnketaBolnica.OcenaOprema = BasicRatingBarEq.Value;
-            HospitalGradeRepository.Instance.Create(AnketaBolnica);
+            HospitalGrade anketaBolnica = new HospitalGrade
+            {
+                Id = Guid.NewGuid().ToString("N"),
+                OcenaCistoca = BasicRatingBarClean.Value,
+                OcenaDostupnost = BasicRatingBarAvailable.Value,
+                OcenaLakoca = BasicRatingBarEasy.Value,
+                OcenaOprema =  BasicRatingBarEq.Value
+            };
         }
-
-        /*private bool CanExecuteRate()
-        {
-            return false;
-        }*/
-
-
-        /*private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AnketaBolnica.Id = Guid.NewGuid().ToString("N");
-            //Patient.Id = Guid.NewGuid().ToString("N");
-            AnketaBolnica.OcenaCistoca = BasicRatingBarClean.Value;
-            AnketaBolnica.OcenaDostupnost = BasicRatingBarAvailable.Value;
-            AnketaBolnica.OcenaLakoca = BasicRatingBarEasy.Value;
-            AnketaBolnica.OcenaOprema = BasicRatingBarEq.Value;
-            HospitalGradeRepository.Instance.Create(AnketaBolnica);    
-        }*/
     }
 }
