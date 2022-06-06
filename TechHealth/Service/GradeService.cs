306c4981d7dfc5ec0290d553ac3071d7a0903d41
Nenad Sredojević <n.sredojevic3@gmail.com>
2022-06-06 16:49:34 +0200

    Surveys work

---
+++ TechHealth/Service/GradeService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHealth.Model;
+using TechHealth.Repository;
+
+namespace TechHealth.Service
+{
+    public class GradeService
+    {
+        public AppointmentGrade GetById(string id)
+        {
+            return GradeRepository.Instance.GetById(id);
+        }
+
+        public List<AppointmentGrade> GetAll()
+        {
+            return GradeRepository.Instance.GetAllToList();
+        }
+
+        public bool Create(AppointmentGrade grade)
+        {
+            return GradeRepository.Instance.Create(grade);
+        }
+
+        public bool Update(AppointmentGrade grade)
+        {
+            return GradeRepository.Instance.Update(grade);
+        }
+
+        public bool Delete(string id)
+        {
+            return GradeRepository.Instance.Delete(id);
+        }
+
+        public List<int> GetStaffGrades()
+        {
+            List<int> staffGrades = new List<int>();
+            foreach (var grade in GetAll())
+            {
+                staffGrades.Add(grade.OcenaOsoblje);
+            }
+            return staffGrades;
+        }
+
+        public double GetGradesSum(List<int> grades)
+        {
+            double sum = 0;
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+            return sum;
+        }
+
+        public int GetGradesNum(int gradeValue)
+        {
+            int gradeValueCount = 0;
+            foreach (var grade in GetStaffGrades())
+            {
+                if (grade == gradeValue)
+                {
+                    gradeValueCount += 1;
+                }
+            }
+            return gradeValueCount;
+        }
+    }
+}
--- TechHealth/View/ManagerView/ViewModel/SurveyViewModel.cs
+++ TechHealth/View/ManagerView/ViewModel/SurveyViewModel.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechHealth.Controller;
 
 namespace TechHealth.View.ManagerView.ViewModel
 {
     public class SurveyViewModel
     {
+        private GradeController gradeController = new GradeController();
+        private List<int> staffGrades;
+        public string FiveStarStaffGrade { get; set; }
+        public string FourStarStaffGrade { get; set; }
+        public string ThreeStarStaffGrade { get; set; }
+        public string TwoStarStaffGrade { get; set; }
+        public string OneStarStaffGrade { get; set; }
+
+
+        public SurveyViewModel()
+        {
+            staffGrades = gradeController.GetStaffGrades();
+            FiveStarStaffGrade = "(" + gradeController.GetGradesNum(5) + ")";
+            FourStarStaffGrade = "(" + gradeController.GetGradesNum(4) + ")";
+            ThreeStarStaffGrade = "(" + gradeController.GetGradesNum(3) + ")";
+            TwoStarStaffGrade = "(" + gradeController.GetGradesNum(2) + ")";
+            OneStarStaffGrade = "(" + gradeController.GetGradesNum(1) + ")";
+        }
     }
 }
