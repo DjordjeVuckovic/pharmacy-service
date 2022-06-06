9906a8f5d5056e1d78e7a5dbb5de02c738a7e0ec
Nenad Sredojević <n.sredojevic3@gmail.com>
2022-06-06 15:21:22 +0200

    Fixing view models

--- TechHealth/View/ManagerView/VieW/RoomView.xaml.cs
+++ TechHealth/View/ManagerView/VieW/RoomView.xaml.cs
@@ -30,184 +30,189 @@ namespace TechHealth.View.ManagerView.VieW
     /// <summary>
     /// Interaction logic for RoomView.xaml
     /// </summary>
-    public partial class RoomView : UserControl, INotifyPropertyChanged
+    public partial class RoomView : UserControl/*, INotifyPropertyChanged*/
     {
-        private ObservableCollection<Room> rooms;
-
-        private RoomController roomController = new RoomController();
-        private RoomMergingController roomMergingController = new RoomMergingController();
-        private RoomSeparationController roomSeparationController = new RoomSeparationController();
-        private Room selectedItem;
-        public event PropertyChangedEventHandler PropertyChanged;
-        public static Timer timer;
-        public static Timer timer1;
-        public RelayCommand AddRoomCommand { get; set; }
-        public RelayCommand DeleteRoomCommand { get; set; }
-        public RelayCommand UpdateRoomCommand { get; set; }
-        public RelayCommand InventoryCommand { get; set; }
-        public RelayCommand RenovationCommand { get; set; }
-        public RelayCommand MergeRoomsCommand { get; set; }
-        public RelayCommand SeparateRoomsCommand { get; set; }
-        public Room SelectedItem
-        {
-            get
-            {
-                return selectedItem;
-            }
-            set
-            {
-                selectedItem = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public ObservableCollection<Room> Rooms
-        {
-            get
-            {
-                return rooms;
-            }
-
-            set
-            {
-                rooms = value;
-                OnPropertyChanged(nameof(Rooms));
-            }
-        }
         public RoomView()
         {
-            timer = new Timer(new TimerCallback(roomMergingController.MergeOnDate), null, 1000, 60000);
-            //timer1 = new Timer(new TimerCallback(roomSeparationController.SeparateOnDate), null, 1000, 60000);
             InitializeComponent();
-            DataContext = this;
-            rooms = new ObservableCollection<Room>(roomController.GetAll());
-            AddRoomCommand = new RelayCommand(param => ExecuteAdd(), param => CanExecuteAdd());
-            DeleteRoomCommand = new RelayCommand(param => ExecuteDel(), param => CanExecuteDel());
-            UpdateRoomCommand = new RelayCommand(param => ExecuteUpdate(), param => CanExecuteUpdate());
-            InventoryCommand = new RelayCommand(param => ExecuteInventory(), param => CanExecuteInventory());
-            RenovationCommand = new RelayCommand(param => ExecuteRenovation(), param => CanExecuteRenovation());
-            MergeRoomsCommand = new RelayCommand(param => ExecuteMerge(), param => CanExecuteMerge());
-            SeparateRoomsCommand = new RelayCommand(param => ExecuteSeparation(), param => CanExecuteSeparation());
+            DataContext = new RoomViewModel();
         }
+        //private ObservableCollection<Room> rooms;
 
-        private bool CanExecuteSeparation()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private RoomController roomController = new RoomController();
+        //private RoomMergingController roomMergingController = new RoomMergingController();
+        //private RoomSeparationController roomSeparationController = new RoomSeparationController();
+        //private Room selectedItem;
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //public static Timer timer;
+        //public static Timer timer1;
+        //public RelayCommand AddRoomCommand { get; set; }
+        //public RelayCommand DeleteRoomCommand { get; set; }
+        //public RelayCommand UpdateRoomCommand { get; set; }
+        //public RelayCommand InventoryCommand { get; set; }
+        //public RelayCommand RenovationCommand { get; set; }
+        //public RelayCommand MergeRoomsCommand { get; set; }
+        //public RelayCommand SeparateRoomsCommand { get; set; }
+        //public Room SelectedItem
+        //{
+        //    get
+        //    {
+        //        return selectedItem;
+        //    }
+        //    set
+        //    {
+        //        selectedItem = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
-            return true;
-        }
+        //public ObservableCollection<Room> Rooms
+        //{
+        //    get
+        //    {
+        //        return rooms;
+        //    }
 
-        private void ExecuteSeparation()
-        {
-            //new SeparateRooms(selectedItem).ShowDialog();
-            var SeparateRoomsVm = new SeparateRoomsViewModel(selectedItem);
-            MainViewModel.Instance().CurrentView = SeparateRoomsVm;
-        }
+        //    set
+        //    {
+        //        rooms = value;
+        //        OnPropertyChanged(nameof(Rooms));
+        //    }
+        //}
+        //public RoomView()
+        //{
+        //    timer = new Timer(new TimerCallback(roomMergingController.MergeOnDate), null, 1000, 60000);
+        //    //timer1 = new Timer(new TimerCallback(roomSeparationController.SeparateOnDate), null, 1000, 60000);
+        //    InitializeComponent();
+        //    DataContext = this;
+        //    rooms = new ObservableCollection<Room>(roomController.GetAll());
+        //    AddRoomCommand = new RelayCommand(param => ExecuteAdd(), param => CanExecuteAdd());
+        //    DeleteRoomCommand = new RelayCommand(param => ExecuteDel(), param => CanExecuteDel());
+        //    UpdateRoomCommand = new RelayCommand(param => ExecuteUpdate(), param => CanExecuteUpdate());
+        //    InventoryCommand = new RelayCommand(param => ExecuteInventory(), param => CanExecuteInventory());
+        //    RenovationCommand = new RelayCommand(param => ExecuteRenovation(), param => CanExecuteRenovation());
+        //    MergeRoomsCommand = new RelayCommand(param => ExecuteMerge(), param => CanExecuteMerge());
+        //    SeparateRoomsCommand = new RelayCommand(param => ExecuteSeparation(), param => CanExecuteSeparation());
+        //}
 
-        private bool CanExecuteMerge()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private bool CanExecuteSeparation()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private void ExecuteMerge()
-        {
-            //new MergeRooms(selectedItem).ShowDialog();
-            var MergeVm = new MergeRoomsViewModel(selectedItem);
-            MainViewModel.Instance().CurrentView = MergeVm;
-        }
+        //private void ExecuteSeparation()
+        //{
+        //    //new SeparateRooms(selectedItem).ShowDialog();
+        //    var SeparateRoomsVm = new SeparateRoomsViewModel(selectedItem);
+        //    MainViewModel.Instance().CurrentView = SeparateRoomsVm;
+        //}
 
-        private bool CanExecuteRenovation()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private bool CanExecuteMerge()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private void ExecuteRenovation()
-        {
-            //new AddRenovation(selectedItem).ShowDialog();
-            var RenovationVm = new AddRenovationViewModel(selectedItem);
-            MainViewModel.Instance().CurrentView = RenovationVm;
-        }
+        //private void ExecuteMerge()
+        //{
+        //    //new MergeRooms(selectedItem).ShowDialog();
+        //    var MergeVm = new MergeRoomsViewModel(selectedItem);
+        //    MainViewModel.Instance().CurrentView = MergeVm;
+        //}
 
-        private bool CanExecuteInventory()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private bool CanExecuteRenovation()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private void ExecuteInventory()
-        {
-            //new RoomInventory(selectedItem).ShowDialog();
-            var RoomInventoryVm = new RoomInventoryViewModel(selectedItem);
-            MainViewModel.Instance().CurrentView = RoomInventoryVm;
-        }
+        //private void ExecuteRenovation()
+        //{
+        //    //new AddRenovation(selectedItem).ShowDialog();
+        //    var RenovationVm = new AddRenovationViewModel(selectedItem);
+        //    MainViewModel.Instance().CurrentView = RenovationVm;
+        //}
 
-        private bool CanExecuteUpdate()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private bool CanExecuteInventory()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private void ExecuteUpdate()
-        {
-            //new UpdateForm(selectedItem).ShowDialog();
-            var UpdateRoomVm = new UpdateRoomViewModel(selectedItem);
-            MainViewModel.Instance().CurrentView = UpdateRoomVm;
-        }
+        //private void ExecuteInventory()
+        //{
+        //    //new RoomInventory(selectedItem).ShowDialog();
+        //    var RoomInventoryVm = new RoomInventoryViewModel(selectedItem);
+        //    MainViewModel.Instance().CurrentView = RoomInventoryVm;
+        //}
 
-        private bool CanExecuteDel()
-        {
-            if (selectedItem == null)
-            {
-                return false;
-            }
+        //private bool CanExecuteUpdate()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private void ExecuteDel()
-        {
-            roomController.Delete(selectedItem.RoomId);
-            rooms.Remove(selectedItem);
-            MessageBox.Show("You have successfully deleted the room");
-        }
+        //private void ExecuteUpdate()
+        //{
+        //    //new UpdateForm(selectedItem).ShowDialog();
+        //    var UpdateRoomVm = new UpdateRoomViewModel(selectedItem);
+        //    MainViewModel.Instance().CurrentView = UpdateRoomVm;
+        //}
 
-        private bool CanExecuteAdd()
-        {
-            return true;
-        }
+        //private bool CanExecuteDel()
+        //{
+        //    if (selectedItem == null)
+        //    {
+        //        return false;
+        //    }
 
-        private void ExecuteAdd()
-        {
-            //new AddForm(rooms).ShowDialog();
-            var AddRoomVm = new AddRoomViewModel();
-            MainViewModel.Instance().CurrentView = AddRoomVm;
-        }
+        //    return true;
+        //}
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        //private void ExecuteDel()
+        //{
+        //    roomController.Delete(selectedItem.RoomId);
+        //    rooms.Remove(selectedItem);
+        //    MessageBox.Show("You have successfully deleted the room");
+        //}
+
+        //private bool CanExecuteAdd()
+        //{
+        //    return true;
+        //}
+
+        //private void ExecuteAdd()
+        //{
+        //    //new AddForm(rooms).ShowDialog();
+        //    var AddRoomVm = new AddRoomViewModel();
+        //    MainViewModel.Instance().CurrentView = AddRoomVm;
+        //}
+
+        //[NotifyPropertyChangedInvocator]
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
--- TechHealth/View/ManagerView/ViewModel/RoomViewModel.cs
+++ TechHealth/View/ManagerView/ViewModel/RoomViewModel.cs
@@ -1,12 +1,193 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using TechHealth.Controller;
+using TechHealth.Core;
+using TechHealth.Model;
 
 namespace TechHealth.View.ManagerView.ViewModel
 {
     public class RoomViewModel
     {
+        private ObservableCollection<Room> rooms;
+
+        private RoomController roomController = new RoomController();
+        private RoomMergingController roomMergingController = new RoomMergingController();
+        private RoomSeparationController roomSeparationController = new RoomSeparationController();
+        private Room selectedItem;
+        //public event PropertyChangedEventHandler PropertyChanged;
+        public static Timer timer;
+        public static Timer timer1;
+        public RelayCommand AddRoomCommand { get; set; }
+        public RelayCommand DeleteRoomCommand { get; set; }
+        public RelayCommand UpdateRoomCommand { get; set; }
+        public RelayCommand InventoryCommand { get; set; }
+        public RelayCommand RenovationCommand { get; set; }
+        public RelayCommand MergeRoomsCommand { get; set; }
+        public RelayCommand SeparateRoomsCommand { get; set; }
+        public Room SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                selectedItem = value;
+                //OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Room> Rooms
+        {
+            get
+            {
+                return rooms;
+            }
+
+            set
+            {
+                rooms = value;
+                //OnPropertyChanged(nameof(Rooms));
+            }
+        }
+        public RoomViewModel()
+        {
+            timer = new Timer(new TimerCallback(roomMergingController.MergeOnDate), null, 1000, 60000);
+            //timer1 = new Timer(new TimerCallback(roomSeparationController.SeparateOnDate), null, 1000, 60000);
+            rooms = new ObservableCollection<Room>(roomController.GetAll());
+            AddRoomCommand = new RelayCommand(param => ExecuteAdd(), param => CanExecuteAdd());
+            DeleteRoomCommand = new RelayCommand(param => ExecuteDel(), param => CanExecuteDel());
+            UpdateRoomCommand = new RelayCommand(param => ExecuteUpdate(), param => CanExecuteUpdate());
+            InventoryCommand = new RelayCommand(param => ExecuteInventory(), param => CanExecuteInventory());
+            RenovationCommand = new RelayCommand(param => ExecuteRenovation(), param => CanExecuteRenovation());
+            MergeRoomsCommand = new RelayCommand(param => ExecuteMerge(), param => CanExecuteMerge());
+            SeparateRoomsCommand = new RelayCommand(param => ExecuteSeparation(), param => CanExecuteSeparation());
+        }
+
+        private bool CanExecuteSeparation()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteSeparation()
+        {
+            //new SeparateRooms(selectedItem).ShowDialog();
+            var SeparateRoomsVm = new SeparateRoomsViewModel(selectedItem);
+            MainViewModel.Instance().CurrentView = SeparateRoomsVm;
+        }
+
+        private bool CanExecuteMerge()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteMerge()
+        {
+            //new MergeRooms(selectedItem).ShowDialog();
+            var MergeVm = new MergeRoomsViewModel(selectedItem);
+            MainViewModel.Instance().CurrentView = MergeVm;
+        }
+
+        private bool CanExecuteRenovation()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteRenovation()
+        {
+            //new AddRenovation(selectedItem).ShowDialog();
+            var RenovationVm = new AddRenovationViewModel(selectedItem);
+            MainViewModel.Instance().CurrentView = RenovationVm;
+        }
+
+        private bool CanExecuteInventory()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteInventory()
+        {
+            //new RoomInventory(selectedItem).ShowDialog();
+            var RoomInventoryVm = new RoomInventoryViewModel(selectedItem);
+            MainViewModel.Instance().CurrentView = RoomInventoryVm;
+        }
+
+        private bool CanExecuteUpdate()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteUpdate()
+        {
+            //new UpdateForm(selectedItem).ShowDialog();
+            var UpdateRoomVm = new UpdateRoomViewModel(selectedItem);
+            MainViewModel.Instance().CurrentView = UpdateRoomVm;
+        }
+
+        private bool CanExecuteDel()
+        {
+            if (selectedItem == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ExecuteDel()
+        {
+            roomController.Delete(selectedItem.RoomId);
+            rooms.Remove(selectedItem);
+            MessageBox.Show("You have successfully deleted the room");
+        }
+
+        private bool CanExecuteAdd()
+        {
+            return true;
+        }
+
+        private void ExecuteAdd()
+        {
+            //new AddForm(rooms).ShowDialog();
+            var AddRoomVm = new AddRoomViewModel();
+            MainViewModel.Instance().CurrentView = AddRoomVm;
+        }
+
+        //[NotifyPropertyChangedInvocator]
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
