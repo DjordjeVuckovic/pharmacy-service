2724d53b35eb66235dcc052827de31052973fb0a
igorr00 <igor.miskic07@gmail.com>
2022-06-08 16:12:39 +0200

    refactoring 1

---
+++ TechHealth/Service/MeetingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechHealth.Model;
+using TechHealth.Repository;
+
+namespace TechHealth.Service
+{
+    public class MeetingService
+    {
+        public bool Create(Meeting meeting)
+        {
+            return MeetingRepository.Instance.Create(meeting);
+        }
+        public bool Delete(string id)
+        {
+            return MeetingRepository.Instance.Delete(id);
+        }
+        public bool CheckAttendants(Meeting meeting)
+        {
+            return MeetingRepository.Instance.CheckAttendants(meeting);
+        }
+    }
+}
