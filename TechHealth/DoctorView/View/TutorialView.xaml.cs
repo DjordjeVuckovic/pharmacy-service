04d3128e870cd707ddf7361eac8131a0c3507e93
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-06-12 16:39:25 +0200

    tutorial implemented

---
+++ TechHealth/DoctorView/View/TutorialView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TechHealth.DoctorView.View
+{
+    public partial class TutorialView : UserControl
+    {
+        public TutorialView()
+        {
+            InitializeComponent();
+        }
+    }
+}
