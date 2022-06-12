be763e992288706ccd55581ef389a107e4e838f3
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-06-12 17:07:30 +0200

    added new buttons for tutorial

--- TechHealth/DoctorView/View/TutorialView.xaml.cs
+++ TechHealth/DoctorView/View/TutorialView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace TechHealth.DoctorView.View
 {
@@ -7,6 +8,21 @@ namespace TechHealth.DoctorView.View
         public TutorialView()
         {
             InitializeComponent();
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Play();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+           MediaElement.Stop();
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Pause();
         }
     }
 }
